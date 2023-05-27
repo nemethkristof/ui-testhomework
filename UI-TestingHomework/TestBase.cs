491412e9a177e2301990ad162e521895d741e6d5
csokispalacsinta <44522566+csokispalacsinta@users.noreply.github.com>
2023-05-27 17:17:58 +0200

    Add TestBase class

---
+++ UI-TestingHomework/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+
+namespace UI_TestingHomework
+{
+    [TestFixture]
+    public class TestBase
+    {
+        IWebDriver driver;
+
+        public IWebDriver Driver
+        {
+            get
+            { return driver; }
+            set
+            {
+                driver.Quit();
+                driver = value;
+            }
+        }
+
+        [SetUp]
+        protected void Setup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--lang=hu");
+
+            driver = new ChromeDriver(options);
+        }
+
+        [TearDown]
+        protected void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
